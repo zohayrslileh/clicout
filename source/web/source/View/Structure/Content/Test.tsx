2a651f3a0fdb6dcaf5da53f4a2f4719b25bf737e
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-01 18:56:03 +0100

    save

--- source/web/source/View/Structure/Content/Test.tsx
+++ source/web/source/View/Structure/Content/Test.tsx
@@ -1,6 +1,6 @@
 import manager from "@/Models/Server/Socket"
-import { useEffect, useRef } from "react"
 import styled from "@emotion/styled"
+import { useRef } from "react"
 
 /**
  * Test
@@ -28,35 +28,13 @@ export default function () {
     stream.useConnected()
 
     /**
-     * Before Effect
+     * On data
      * 
      */
-    useEffect(function () {
+    stream.useOn("data", function (buffer: ArrayBuffer) {
 
-        // Check video
-        if (!video.current) throw new Error("Vedio tag was not found")
-
-        // Create media source
-        const mediaSource = new MediaSource()
-
-        // Set vedio source
-        video.current.src = URL.createObjectURL(mediaSource)
-
-        // On source open
-        mediaSource.addEventListener("sourceopen", function () {
-
-            // Source buffer
-            const sourceBuffer = mediaSource.addSourceBuffer(`video/mp4; codecs="avc1.42E01E, mp4a.40.2"`)
-
-            // On data
-            stream.socket.on("data", function (data: ArrayBuffer) {
-
-                // Append to source buffer
-                sourceBuffer.appendBuffer(new Uint8Array(data))
-            })
-        })
-
-    }, [])
+        console.log(URL.createObjectURL(new Blob([buffer])))
+    })
 
     return <Container>
         <video ref={video} controls />
