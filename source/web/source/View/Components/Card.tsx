26e04fd855bfe957e035f0a160dfb1dedac26ecf
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-03 17:35:35 +0100

    save

--- source/web/source/View/Components/Card.tsx
+++ source/web/source/View/Components/Card.tsx
@@ -6,7 +6,8 @@ import styled from "@emotion/styled"
  * 
  */
 export default styled.div`
-    background-color: ${() => Appearance.theme.schema.BACKGROUND_SECONDARY.hex};
+    background-color: ${() => Appearance.theme.schema.BACKGROUND_SECONDARY.rgba(0.5)};
+    backdrop-filter: blur(5px);
     border-radius: 10px;
     padding: 10px;
 `
