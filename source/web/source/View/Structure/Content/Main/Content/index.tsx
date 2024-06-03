f5175023f89034e203d0aa7de9f527934edf8048
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-03 14:41:38 +0100

    Route

--- source/web/source/View/Structure/Content/Main/Content/index.tsx
+++ source/web/source/View/Structure/Content/Main/Content/index.tsx
@@ -1,14 +1,10 @@
 import PendingException from "@/View/Exception/Exceptions/Pending"
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import { Throw } from "@/Tools/Exception"
 import Card from "@/View/Components/Card"
 import Exception from "@/View/Exception"
-import { Suspense, lazy } from "react"
 import styled from "@emotion/styled"
-
-const Participant = lazy(() => import("./Participant"))
-const Session = lazy(() => import("./Session"))
-const Judge = lazy(() => import("./Judge"))
+import { Suspense } from "react"
 
 /**
  * Content
@@ -24,10 +20,7 @@ export default function () {
             <Suspense fallback={<Throw exception={new PendingException} />}>
 
                 <Routes>
-                    <Route index element={<Navigate to="session" />} />
-                    <Route path="participant/*" element={<Participant />} />
-                    <Route path="session/*" element={<Session />} />
-                    <Route path="judge/*" element={<Judge />} />
+                    <Route index element={<h1>Hello</h1>} />
                 </Routes>
 
             </Suspense>
