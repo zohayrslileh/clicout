2d4839958442deeedfaf06999473a7136257bd41
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-04 19:05:53 +0100

    subscription

--- source/web/source/View/Structure/Content/Main/Content/Home.tsx
+++ source/web/source/View/Structure/Content/Main/Content/Home.tsx
@@ -2,6 +2,9 @@ import JsonView from "@/View/Components/JsonView"
 import styled from "@emotion/styled"
 import { useContext } from "react"
 import User from "@/Core/User"
+import PendingException from "@/View/Exception/Exceptions/Pending"
+import { Throw } from "@/Tools/Exception"
+import usePromise from "@/Tools/Promise"
 
 /**
  * Content
@@ -16,8 +19,21 @@ export default function () {
      */
     const user = useContext(User.context)
 
+    /**
+     * Subscription
+     * 
+     */
+    const subscription = usePromise(async () => await user.subscription(), [])
+
+    // Pending status
+    if (subscription.pending) return <Throw exception={new PendingException} />
+
+    // Exception status
+    if (subscription.exception) return <Throw exception={subscription.exception.current} />
+
     return <Container>
         <JsonView json={user} />
+        {subscription.solve ? <JsonView json={subscription.solve} /> : "You don't have subscription"}
     </Container>
 }
 
