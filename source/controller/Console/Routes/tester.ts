88a1ee229723683c25e02de16bd6009801597b33
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-04 23:35:29 +0100

    Tester 🧪

--- source/controller/Console/Routes/tester.ts
+++ source/controller/Console/Routes/tester.ts
@@ -1,4 +1,3 @@
-import Json from "@/Tools/Json"
 
 /*
 |-----------------------------
@@ -9,27 +8,5 @@ import Json from "@/Tools/Json"
 */
 export default async function () {
 
-    const cities = new Json<City[]>("assets/database/cities.json")
-    const countries = new Json<Country[]>("assets/database/countries.json")
-
-    cities.value = cities.value
-
-    console.log(countries.value.length)
-
     console.log("The test completed successfully 🧪 ")
-}
-
-interface City {
-    id: string
-    name: string
-    country: string
-    admin1: string
-    lat: string
-    lon: string
-    pop: string
-}
-
-interface Country {
-    name: string
-    code: string
 }
