e8ef19317097ff9c72a7d2a1fc6851767d0092c8
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-04 18:10:37 +0100

    primitivePlans

--- source/controller/Models/Database/Migration.ts
+++ source/controller/Models/Database/Migration.ts
@@ -1,3 +1,5 @@
+import Plan from "./Entities/Plan"
+import Json from "@/Tools/Json"
 
 /*
 |-----------------------------
@@ -7,4 +9,23 @@
 |
 */
 export default async function () {
+
+    // Primitive plans
+    const primitivePlans = new Json<unknown[]>("assets/database/plans.json")
+
+    // Fetch primitive plans
+    for (const primitivePlan of primitivePlans.value) {
+
+        // Initialize plan
+        const plan = new Plan
+
+        // Set primitive plan
+        Object.assign(plan, primitivePlan)
+
+        // Check if is exists
+        if (await Plan.findOneBy({ name: plan.name })) continue
+
+        // Save
+        await plan.save()
+    }
 }
