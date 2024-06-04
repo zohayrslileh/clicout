61c32c9edec45fb7ef54211c112247435a85ff8c
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-04 21:39:26 +0100

    City

---
+++ source/controller/Models/Database/Entities/City.ts
@@ -0,0 +1,13 @@
+import BaseEntity from "@/Tools/Database/Entity"
+import { Entity } from "typeorm"
+
+/*
+|-----------------------------
+|  City model
+|-----------------------------
+|
+| 
+*/
+@Entity()
+export default class City extends BaseEntity {
+}
---
+++ source/controller/Models/Database/Entities/Country.ts
@@ -0,0 +1,13 @@
+import BaseEntity from "@/Tools/Database/Entity"
+import { Entity } from "typeorm"
+
+/*
+|-----------------------------
+|  Country model
+|-----------------------------
+|
+| 
+*/
+@Entity()
+export default class Country extends BaseEntity {
+}
