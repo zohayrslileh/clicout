8b44c1faf58a73268e498a43b0f2ae13089c5010
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-05 12:16:14 +0100

    save

--- source/web/source/View/Structure/Content/Routes/index.tsx
+++ source/web/source/View/Structure/Content/Routes/index.tsx
@@ -2,6 +2,7 @@ import PendingException from "@/View/Exception/Exceptions/Pending"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { Throw } from "@/Tools/Exception"
 import { lazy, Suspense } from "react"
+import User from "@/Core/User"
 
 const Auth = lazy(() => import("./Auth"))
 const Main = lazy(() => import("./Main"))
@@ -13,12 +14,24 @@ const Main = lazy(() => import("./Main"))
  */
 export default function () {
 
+    /**
+     * Authentication
+     * 
+     */
+    const authentication = User.useAuthentication()
+
+    // Pending status
+    if (authentication.pending) return <Throw exception={new PendingException} />
+
+    // Exception status
+    if (authentication.exception && !authentication.unauthorized) return <Throw exception={authentication.exception.current} />
+
     return <Suspense fallback={<Throw exception={new PendingException} />}>
 
         <Routes>
             <Route index element={<Navigate to="/main" />} />
-            <Route path="/main/*" element={<Main />} />
-            <Route path="/auth/*" element={<Auth />} />
+            <Route path="/main/*" element={authentication.unauthorized ? <Navigate to="/auth" /> : <Main />} />
+            <Route path="/auth/*" element={authentication.unauthorized ? <Auth /> : <Navigate to="/main" />} />
         </Routes>
 
     </Suspense>
