4b12af715952ed186a8c0dcce82a5e298d62fe98
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-05 09:57:54 +0100

    Plans

--- source/web/source/View/Structure/Content/Main/Routes/Upgrade/index.tsx
+++ source/web/source/View/Structure/Content/Main/Routes/Upgrade/index.tsx
@@ -1,4 +1,6 @@
+import { Lang } from "@/Tools/Language"
 import styled from "@emotion/styled"
+import User from "@/Core/User"
 
 /**
  * Upgrade
@@ -7,8 +9,16 @@ import styled from "@emotion/styled"
  */
 export default function () {
 
+    /**
+     * User
+     * 
+     */
+    const user = User.useContext()
+
     return <Container>
-        Upgrade
+        <h1><Lang>Welcome</Lang>, {user.username}</h1>
+        <p>Please choose you plan</p>
+        <p>Plans</p>
     </Container>
 }
 
