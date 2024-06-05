b6e07da97843c40ee296bb44a6460c36506b38bc
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-05 12:30:41 +0100

    Dashboard

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/index.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/index.tsx
@@ -1,49 +1,25 @@
-import PendingException from "@/View/Exception/Exceptions/Pending"
-import { Navigate } from "react-router-dom"
-import { Throw } from "@/Tools/Exception"
 import styled from "@emotion/styled"
 import Content from "./Content"
 import Sidebar from "./Sidebar"
-import User from "@/Core/User"
 import Navbar from "./Navbar"
 
 /**
- * Main
+ * Dashboard
  * 
  * @returns 
  */
 export default function () {
 
-    /**
-     * Authentication
-     * 
-     */
-    const authentication = User.useAuthentication()
-
-    // Pending status
-    if (authentication.pending) return <Throw exception={new PendingException} />
-
-    // Unauthorized status
-    if (authentication.unauthorized) return <Navigate to="/auth" />
-
-    // Exception status
-    if (authentication.exception) return <Throw exception={authentication.exception.current} />
-
-    // Authorized status
     return <Container>
 
-        <User.context.Provider value={authentication.solve}>
+        {/** Navbar */}
+        <Navbar />
 
-            {/** Navbar */}
-            <Navbar />
+        {/** Sidebar */}
+        <Sidebar />
 
-            {/** Sidebar */}
-            <Sidebar />
-
-            {/** Content */}
-            <Content />
-
-        </User.context.Provider>
+        {/** Content */}
+        <Content />
 
     </Container>
 }
