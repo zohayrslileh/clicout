e7f6817396f4becdf1cc9d05525b504836fa49a6
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-05 10:14:22 +0100

    Plans

--- source/web/source/View/Structure/Content/Main/Routes/Upgrade/index.tsx
+++ source/web/source/View/Structure/Content/Main/Routes/Upgrade/index.tsx
@@ -1,7 +1,10 @@
+import PendingException from "@/View/Exception/Exceptions/Pending"
+import { Throw } from "@/Tools/Exception"
+import usePromise from "@/Tools/Promise"
 import { Lang } from "@/Tools/Language"
 import styled from "@emotion/styled"
 import User from "@/Core/User"
-import Plans from "./Plans"
+import Plan from "@/Core/Plan"
 
 /**
  * Upgrade
@@ -16,10 +19,22 @@ export default function () {
      */
     const user = User.useContext()
 
+    /**
+     * Plans promise
+     * 
+     */
+    const plans = usePromise(async () => await Plan.find(), [])
+
+    // Pending status
+    if (plans.pending) return <Throw exception={new PendingException} />
+
+    // Exception status
+    if (plans.exception) return <Throw exception={plans.exception.current} />
+
     return <Container>
         <h1><Lang>Welcome</Lang>, {user.username}</h1>
         <p>Please choose you plan</p>
-        <Plans />
+        Plans: {plans.solve.length}
     </Container>
 }
 
