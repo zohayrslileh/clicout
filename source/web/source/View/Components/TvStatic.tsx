db3271b849f0514c1a8226ca0469121e5d34f0f0
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-06 17:27:54 +0100

    save

--- source/web/source/View/Components/TvStatic.tsx
+++ source/web/source/View/Components/TvStatic.tsx
@@ -7,13 +7,20 @@ import React from "react"
  * 
  * @returns 
  */
-export default function ({ ...props }: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) {
+export default function ({ children, ...props }: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) {
 
     /**
      * Container
      * 
      */
-    return <Container {...props} />
+    return <Container {...props}>
+        <div className="layer1">
+            <div className="tv-static animation1"></div>
+        </div>
+        <div className="layer2">
+            <div className="tv-static animation2"></div>
+        </div>
+    </Container>
 }
 
 /**
@@ -21,19 +28,54 @@ export default function ({ ...props }: React.DetailedHTMLProps<React.HTMLAttribu
  * 
  */
 const Container = styled.div`
-    width: 500px;
-    height: 300px;
-    margin: auto;
-    background-image: repeating-conic-gradient(white, black 0.00085%);
-    animation: ${keyframes`
+    display: flex;
+    position: absolute;
+    width: 100%;
+    height: 100%;
 
-        from {
-            background-size: 100% 100%;
-        }
+    .layer1 {
+        width: 100%;
+        background-color: rgba(255, 255, 255, 0.5);
+    }
 
-        to {
-            background-size: 200% 200%;
-        }
-        
-    `} 25s linear infinite;
+    .layer2 {
+        width: 100%;
+        margin-left: -100%;
+        background-color: rgba(255, 255, 255, 0.5);
+    }
+
+    .tv-static {
+        width: 100%;
+        height: 100%;
+        margin: auto;
+        background-image: repeating-radial-gradient(circle at 17% 32%, white, black 0.00085px);
+    }
+
+    .animation1 {
+        animation: ${keyframes`
+
+            from {
+                background-size: 100% 100%;
+            } 
+
+            to {
+                background-size: 99% 100%;
+            }
+
+        `} 1s linear infinite;
+    }
+
+    .animation2 {
+        animation: ${keyframes`
+
+            from {
+                background-size: 48.56% 50%;
+            }
+
+            to {
+                background-size: 43.9% 50.1%;
+            }
+      
+        `} 0.1s linear infinite;
+    }
 `
