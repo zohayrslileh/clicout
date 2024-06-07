e310795eebe62544a86623115412ff8422ef17de
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-07 20:47:03 +0100

    save

--- source/controller/Models/Database/Migration.ts
+++ source/controller/Models/Database/Migration.ts
@@ -1,3 +1,5 @@
+import City from "./Entities/City"
+import Country from "./Entities/Country"
 import Plan from "./Entities/Plan"
 import Json from "@/Tools/Json"
 
@@ -10,22 +12,72 @@ import Json from "@/Tools/Json"
 */
 export default async function () {
 
-    // Primitive plans
-    const primitivePlans = new Json<unknown[]>("assets/database/plans.json")
+    // Migration plans
+    if (!await Plan.count()) {
 
-    // Fetch primitive plans
-    for (const primitivePlan of primitivePlans.value) {
+        // Primitive plans
+        const primitivePlans = new Json<unknown[]>("assets/database/plans.json")
 
-        // Initialize plan
-        const plan = new Plan
+        // Fetch primitive plans
+        for (const primitivePlan of primitivePlans.value) {
 
-        // Set primitive plan
-        Object.assign(plan, primitivePlan)
+            // Initialize plan
+            const plan = new Plan
 
-        // Check if is exists
-        if (await Plan.findOneBy({ name: plan.name })) continue
+            // Set primitive plan
+            Object.assign(plan, primitivePlan)
 
-        // Save
-        await plan.save()
+            // Save
+            await plan.save()
+
+            console.log("- Migration plan: ", plan.name)
+        }
+    }
+
+    // Migration countries
+    if (!await Country.count()) {
+
+        // Primitive countries
+        const primitiveCountries = new Json<unknown[]>("assets/database/countries.json")
+
+        // Fetch primitive countries
+        for (const primitiveCountry of primitiveCountries.value) {
+
+            // Initialize country
+            const country = new Country
+
+            // Set primitive country
+            Object.assign(country, primitiveCountry)
+
+            // Save
+            await country.save()
+
+            console.log("- Migration country: ", country.name)
+        }
+    }
+
+    // Migration cities
+    if (!await City.count()) {
+
+        // Primitive cities
+        const primitiveCities = new Json<unknown[]>("assets/database/cities.json")
+
+        // Fetch primitive cities
+        for (const primitiveCity of primitiveCities.value) {
+
+            // Initialize city
+            const city = new City
+
+            // Set primitive country
+            Object.assign(city, primitiveCity)
+
+            // Set country
+            city.country = await Country.findOneByOrFail({ code: String(city.country) })
+
+            // Save
+            await city.save()
+
+            console.log("- Migration city: ", city.name)
+        }
     }
 }
