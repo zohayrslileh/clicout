3f3100cf4164a01eb6e0594755c2145a1daef01c
Zohayr Slileh <zohayrslileh@gmail.com>
2024-06-08 14:10:28 +0100

    save

--- source/controller/Console/Routes/tester.ts
+++ source/controller/Console/Routes/tester.ts
@@ -1,3 +1,5 @@
+import Json from "@/Tools/Json"
+import axios from "axios"
 
 /*
 |-----------------------------
@@ -8,5 +10,40 @@
 */
 export default async function () {
 
+    const file = new Json("storage/suggestions.json")
+
+    const suggestions: Suggestion[] = []
+
+    for (const alphabet of alphabets) {
+
+        const response = await axios({
+            url: `https://www.teepublic.com/search/autocomplete?prefix=${alphabet}&limit=200`,
+            headers: {
+                "Accept": "application/json"
+            }
+        })
+
+        suggestions.push(response.data.suggestions)
+
+        console.log("- Fetch: ", alphabet)
+    }
+
+    file.update(suggestions)
+
     console.log("The test completed successfully 🧪 ")
+}
+
+/**
+ * Alphabets
+ *
+ */
+const alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+/**
+ * Suggestion
+ * 
+ */
+interface Suggestion {
+    suggestion: string
+    weight: number
 }
