584377d1a450c1bd34c5b0ef4f21b937f8a79678
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-03 13:52:23 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Auth/Login.tsx
+++ source/web/source/View/Structure/Content/Routes/Auth/Login.tsx
@@ -1,4 +1,8 @@
-import styled from "@emotion/styled"
+import { Button, Card, Image, Input } from "@nextui-org/react"
+import { Lang, useLang } from "@/Tools/Language"
+import Logo from "@/View/Components/Logo"
+import { Link } from "react-router-dom"
+import { Form } from "@/Tools/Form"
 
 /**
  * Login
@@ -7,16 +11,22 @@ import styled from "@emotion/styled"
  */
 export default function () {
 
-    return <Container>
+    /**
+     * Lang
+     * 
+     */
+    const lang = useLang()
 
-        <h1>Login</h1>
-
-    </Container>
+    return <Card className="m-auto p-7 grid grid-cols-2 gap-2">
+        <Image className="h-[400px] w-[400px]" src="https://www.tailwindtap.com/assets/components/form/createaccount/login.svg" alt="Auth Hero" />
+        <div className="grid p-7 gap-10">
+            <Logo className="m-auto" width="200px" />
+            <Form className="grid gap-3">
+                <Input label={lang("Username")} />
+                <Input type="password" label={lang("Password")} />
+                <Button size="lg" color="primary"><Lang>Login</Lang></Button>
+            </Form>
+            <p className="m-auto">Need an account? <Link to="../register" className="text-primary">Sign up</Link></p>
+        </div>
+    </Card>
 }
-
-/**
- * Container
- * 
- */
-const Container = styled.div`
-`
