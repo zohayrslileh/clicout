fa9d6bbda488485c6175b0db455b959988b51ae9
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-03 21:31:47 +0100

    save

--- source/web/source/View/Language/Languages/fr-FR.ts
+++ source/web/source/View/Language/Languages/fr-FR.ts
@@ -10,5 +10,6 @@ export default {
     "Username field is required": "Le champ Nom d'utilisateur est obligatoire",
     "Password field is required": "Le champ Mot de passe est obligatoire",
     "I agree to the terms and conditions": "J'accepte les termes et conditions",
-    "Terms and conditions": "Termes et conditions"
+    "Terms and conditions": "Termes et conditions",
+    "Need an account": "Besoin d'un compte"
 }
