57a40246be9ddd5a413d0c808c9369853df17d71
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-03 21:39:47 +0100

    tarnslate

--- source/web/source/View/Language/Languages/ru-RU.ts
+++ source/web/source/View/Language/Languages/ru-RU.ts
@@ -1,2 +1,15 @@
 export default {
+    "Email": "Электронная почта",
+    "Login": "Авторизоваться",
+    "Username": "Имя пользователя",
+    "Password": "Пароль",
+    "I already have an account": "у меня уже есть аккаунт",
+    "Sign in": "Войти",
+    "Sign up": "Зарегистрироваться",
+    "Email field is required": "Поле электронной почты обязательно",
+    "Username field is required": "Поле имени пользователя обязательно",
+    "Password field is required": "Поле пароля обязательно",
+    "I agree to the terms and conditions": "Я принимаю условия пользовательского соглашения",
+    "Terms and conditions": "Условия и положения",
+    "Need an account": "Нужен аккаунт"
 }
--- source/web/source/View/Language/Languages/zh-CN.ts
+++ source/web/source/View/Language/Languages/zh-CN.ts
@@ -1,2 +1,15 @@
 export default {
+    "Email": "电子邮件",
+    "Login": "登录",
+    "Username": "用户名",
+    "Password": "密码",
+    "I already have an account": "我已经有一个帐户",
+    "Sign in": "登入",
+    "Sign up": "报名",
+    "Email field is required": "电子邮件字段为必填项",
+    "Username field is required": "用户名字段为必填项",
+    "Password field is required": "密码字段为必填项",
+    "I agree to the terms and conditions": "我同意这条款和条件",
+    "Terms and conditions": "条款和条件",
+    "Need an account": "需要一个帐户"
 }
