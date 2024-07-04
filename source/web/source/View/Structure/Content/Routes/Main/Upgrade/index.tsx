9a0d3c04cd8c39c03ff3bf3b3552bf187cfbd931
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-04 16:35:30 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Upgrade/index.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Upgrade/index.tsx
@@ -1,3 +1,5 @@
+import { Route, Routes } from "react-router-dom"
+import Payment from "./Payment"
 import Header from "./Header"
 import Footer from "./Footer"
 import Plans from "./Plans"
@@ -12,7 +14,10 @@ export default function () {
     return (
         <div className="container grid gap-10 p-5 m-auto justify-items-center">
             <Header />
-            <Plans />
+            <Routes>
+                <Route index element={<Plans />} />
+                <Route path=":plan" element={<Payment />} />
+            </Routes>
             <Footer />
         </div>
     )
