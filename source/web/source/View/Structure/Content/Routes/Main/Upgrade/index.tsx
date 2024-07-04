4a64ed89f5770a74e261743a08d32661fdaec6f4
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-04 19:52:07 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Upgrade/index.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Upgrade/index.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom"
+import Exception from "@/View/Exception"
 import Payment from "./Payment"
 import Header from "./Header"
 import Footer from "./Footer"
@@ -14,10 +15,14 @@ export default function () {
     return (
         <div className="container grid gap-10 p-5 m-auto justify-items-center">
             <Header />
-            <Routes>
-                <Route index element={<Plans />} />
-                <Route path=":plan" element={<Payment />} />
-            </Routes>
+            <div className="grid justify-items-center relative w-full min-h-[200px]">
+                <Exception>
+                    <Routes>
+                        <Route index element={<Plans />} />
+                        <Route path=":plan" element={<Payment />} />
+                    </Routes>
+                </Exception>
+            </div>
             <Footer />
         </div>
     )
