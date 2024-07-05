57ccc3fff139d7b9ba98fead69eba925e981e147
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-05 18:41:54 +0100

    Upgrade

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/index.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/index.tsx
@@ -1,6 +1,7 @@
 import Logo from "@/View/Components/Logo"
-import Menu from "./Menu"
 import Card from "@/View/Components/Card"
+import Upgrade from "./Upgrade"
+import Menu from "./Menu"
 
 /**
  * Sidebar
@@ -9,11 +10,13 @@ import Card from "@/View/Components/Card"
  */
 export default function () {
 
-    return <Card className="relative grid grid-rows-[auto_1fr] min-w-[280px] gap-10 overflow-x-hidden overflow-y-auto rounded-sm border-0 border-e border-primary border-opacity-20 dark:border-gray-900">
+    return <Card className="relative grid grid-rows-[auto_1fr_auto] min-w-[280px] gap-10 overflow-x-hidden overflow-y-auto rounded-sm border-0 border-e border-primary border-opacity-20 dark:border-gray-900">
 
         <Logo width={150} className="m-auto pt-10" />
 
         <Menu />
+
+        <Upgrade />
 
     </Card>
 }
