c84f317cb92e02bafee15469b5b7d1c445af0cc3
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-05 18:40:14 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/index.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 import Logo from "@/View/Components/Logo"
 import Menu from "./Menu"
+import Card from "@/View/Components/Card"
 
 /**
  * Sidebar
@@ -8,11 +9,11 @@ import Menu from "./Menu"
  */
 export default function () {
 
-    return <div className="relative grid grid-rows-[auto_1fr] min-w-[280px] gap-10 overflow-x-hidden overflow-y-auto border-e border-primary border-opacity-20 dark:border-gray-900">
+    return <Card className="relative grid grid-rows-[auto_1fr] min-w-[280px] gap-10 overflow-x-hidden overflow-y-auto rounded-sm border-0 border-e border-primary border-opacity-20 dark:border-gray-900">
 
         <Logo width={150} className="m-auto pt-10" />
 
         <Menu />
 
-    </div>
+    </Card>
 }
