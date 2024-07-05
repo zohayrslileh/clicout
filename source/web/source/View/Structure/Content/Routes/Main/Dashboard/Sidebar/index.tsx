2813f689e7fe9269c5ab361048c2824fd697bf5c
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-05 19:24:16 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/index.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/index.tsx
@@ -12,7 +12,7 @@ export default function () {
 
     return <Card className="relative grid grid-rows-[auto_1fr_auto] min-w-[280px] gap-10 overflow-x-hidden overflow-y-auto rounded-sm border-0 border-e border-primary border-opacity-20 dark:border-gray-900">
 
-        <Logo width={150} className="m-auto pt-10" />
+        <Logo width={170} className="m-auto pt-10" />
 
         <Menu />
 
