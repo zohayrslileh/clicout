02b701a7b9176c2370d673d55c3d14b58043e5e5
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-05 14:33:27 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/Menu.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/Menu.tsx
@@ -1,4 +1,4 @@
-import { FcEditImage } from "react-icons/fc"
+import { CiMusicNote1 } from "react-icons/ci"
 import Space from "./Space"
 import Link from "./Link"
 
@@ -12,29 +12,7 @@ export default function () {
     return <nav className="-mx-3 space-y-6 ">
         <Space title="analytics">
             <Link path="/">
-                <FcEditImage className="text-lg" />
-                <span className="mx-2 text-sm font-medium">Dashboard</span>
-            </Link>
-            <Link path="/">
-                <FcEditImage className="text-lg" />
-                <span className="mx-2 text-sm font-medium">Dashboard</span>
-            </Link>
-            <Link path="/">
-                <FcEditImage className="text-lg" />
-                <span className="mx-2 text-sm font-medium">Dashboard</span>
-            </Link>
-        </Space>
-        <Space title="analytics">
-            <Link path="/">
-                <FcEditImage className="text-lg" />
-                <span className="mx-2 text-sm font-medium">Dashboard</span>
-            </Link>
-            <Link path="/">
-                <FcEditImage className="text-lg" />
-                <span className="mx-2 text-sm font-medium">Dashboard</span>
-            </Link>
-            <Link path="/">
-                <FcEditImage className="text-lg" />
+                <CiMusicNote1 className="text-lg" />
                 <span className="mx-2 text-sm font-medium">Dashboard</span>
             </Link>
         </Space>
--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/Space.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/Space.tsx
@@ -9,7 +9,7 @@ export default function ({ title, children }: Props) {
 
     return <nav className="-mx-3 space-y-6 ">
         <div className="space-y-3 ">
-            <label className="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">{title}</label>
+            <label className="px-3 text-xs text-gray-500 uppercase font-medium dark:text-gray-400">{title}</label>
             {children}
         </div>
     </nav>
