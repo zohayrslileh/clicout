c5ad97216c8a4c43ff603d0bd15dd732bc1db256
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-05 16:39:17 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/Link.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/Link.tsx
@@ -15,7 +15,7 @@ export default function ({ path, icon, title, active }: Props) {
      */
     const Icon = icon
 
-    return <Button to={path} variant="light" as={Link} className={`flex group text-opacity-70 ${active ? "bg-[#20dd78] bg-opacity-10 text-opacity-1 text-[#5be49b]" : ""} gap-1 items-center justify-start px-[10px] py-[22px] transition-colors duration-300 transform rounded-md`}>
+    return <Button to={path} variant="light" as={Link} className={`flex group text-opacity-70 ${active ? "bg-[#20dd78] bg-opacity-10 text-opacity-1 text-[#1cb671] dark:text-[#5be49b]" : ""} gap-1 items-center justify-start px-[10px] py-[22px] transition-colors duration-300 transform rounded-md`}>
         <Icon className="text-2xl" />
         <span className="mx-2 text-sm font-medium">{title}</span>
     </Button>
