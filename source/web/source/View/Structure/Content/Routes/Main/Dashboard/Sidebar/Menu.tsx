69c74cef1e2d258e0eef89046630de9c8e234dbe
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-05 13:52:31 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/Menu.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Sidebar/Menu.tsx
@@ -1,4 +1,4 @@
-import { PiPintGlassDuotone } from "react-icons/pi"
+import { PiPlayPauseDuotone } from "react-icons/pi";
 
 /**
  * Menu
@@ -11,7 +11,7 @@ export default function () {
         <div className="space-y-3 ">
             <label className="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">analytics</label>
             <a className="flex items-center p-3 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                <PiPintGlassDuotone />
+                <PiPlayPauseDuotone />
                 <span className="mx-2 text-sm font-medium">Dashboard</span>
             </a>
         </div>
