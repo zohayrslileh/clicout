d1cc41784042de3c93ee10b67f75f60571880285
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-05 10:55:20 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Navbar/index.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Navbar/index.tsx
@@ -6,7 +6,7 @@
  */
 export default function () {
 
-    return <div className="sticky top-0 backdrop-blur-md p-3">
+    return <div className="sticky z-10 top-0 backdrop-blur-md p-3">
         <h1>Navbar</h1>
     </div>
 }
