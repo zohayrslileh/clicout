a6eb44d91ce17cfb3ad924d3aa196d1b50cbf2dc
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-06 13:47:51 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record.tsx
@@ -2,6 +2,7 @@ import { Card } from "@nextui-org/react"
 import { CiFlag1 } from "react-icons/ci"
 import { Link } from "react-router-dom"
 import { Lang } from "@/Tools/Language"
+import Plan from "@/Core/Plan"
 
 /**
  * Record
@@ -10,13 +11,19 @@ import { Lang } from "@/Tools/Language"
  */
 export default function () {
 
+    /**
+     * Plan
+     * 
+     */
+    const plan = Plan.useContext()
+
     return <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
         <Card isPressable as={Link} to="create" className="relative h-[300px] smooth grid place-content-center place-items-center gap-5">
-            <CiFlag1 className="text-3xl" />
-            <p className="uppercase text-sm"><Lang>Create New Attack</Lang></p>
+            <CiFlag1 className="text-3xl text-foreground-500" />
+            <p className="uppercase text-sm font-medium text-foreground-500"><Lang>Create New Attack</Lang></p>
             <div className="absolute right-5 bottom-5 flex items-end gap-1 text-success-500">
                 <p className="text-3xl leading-none font-medium">0</p>
-                <p className="text-sm">/ 2 <Lang>Attack(s)</Lang></p>
+                <p className="text-sm">/ {plan.threads} <Lang>Attack(s)</Lang></p>
             </div>
         </Card>
     </div>
