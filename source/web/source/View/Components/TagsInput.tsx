2765a75df8449f0de5f0336f2d0fdbfd6f52de73
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-06 14:56:35 +0100

    save

---
+++ source/web/source/View/Components/TagsInput.tsx
@@ -0,0 +1,37 @@
+import { TagsInput } from "react-tag-input-component"
+import styled from "@emotion/styled"
+
+/**
+ * Tags Input
+ * 
+ * @returns 
+ */
+export default function ({ ...props }: React.ComponentProps<typeof TagsInput>) {
+
+    return <Container className="h-fit min-h-[200px] relative w-full tap-highlight-transparent shadow-sm px-3 border-medium border-default-200 data-[hover=true]:border-default-400 group-data-[focus=true]:border-default-foreground rounded-medium flex-col items-start justify-center gap-0 transition-background !duration-150 motion-reduce:transition-none py-2">
+        <TagsInput {...props} />
+    </Container>
+}
+
+/**
+ * Container
+ * 
+ */
+const Container = styled.div`
+
+    .rti--container {
+        background-color: transparent;
+        border: none !important;
+        outline: none !important;
+        box-shadow: none !important;
+    }
+
+    .rti--tag {
+        background-color: hsl(var(--nextui-default) / 0.4);
+        padding-inline-start: 12px;
+    }
+    
+    .rti--input {
+        background-color: transparent;
+    }
+`
--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create.tsx
@@ -1,22 +1,16 @@
-import { Code } from "@nextui-org/react";
-import { useState } from "react";
-import { TagsInput } from "react-tag-input-component";
+import TagsInput from "@/View/Components/TagsInput"
+import { useState } from "react"
 
 const Example = () => {
   const [selected, setSelected] = useState(["papaya"]);
 
   return (
-    <div>
-      <h1>Add Fruits</h1>
-      <Code>{JSON.stringify(selected)}</Code>
-      <TagsInput
-        value={selected}
-        onChange={setSelected}
-        name="fruits"
-        placeHolder="enter fruits"
-      />
-      <em>press enter or comma to add new tag</em>
-    </div>
+    <TagsInput
+      value={selected}
+      onChange={setSelected}
+      name="fruits"
+      placeHolder="enter fruits"
+    />
   );
 };
 
