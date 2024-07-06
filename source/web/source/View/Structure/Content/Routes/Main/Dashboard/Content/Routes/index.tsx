e268ef0c261745d0e682d9fbe54f256bbb68c00e
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-06 23:40:04 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/index.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/index.tsx
@@ -12,7 +12,7 @@ const Hub = lazy(() => import("./Hub"))
  */
 export default function () {
 
-    return <div className="relative grid">
+    return <div className="relative grid pt-5">
 
         <Suspense fallback={<Throw exception={new PendingException} />}>
 
