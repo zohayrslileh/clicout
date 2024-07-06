e8c48a886d2108e54c3b9d17697a331bbcd599c7
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-06 17:07:44 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create/index.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create/index.tsx
@@ -4,6 +4,8 @@ import { Lang } from "@/Tools/Language"
 import Country from "@/Core/Country"
 import Countries from "./Countries"
 import { useState } from "react"
+import City from "@/Core/City"
+import Cities from "./Cities"
 
 /**
  * Create
@@ -30,10 +32,17 @@ export default function () {
      */
     const [country, setCountry] = useState<Country | undefined>(undefined)
 
+    /**
+     * City
+     * 
+     */
+    const [city, setCity] = useState<City | undefined>(undefined)
+
     return <Card className="grid smooth gap-5 p-5 max-w-[700px] w-full h-fit mx-auto text-sm bg-content1">
         <h1 className="text-xl font-medium text-foreground-500 uppercase"><Lang>Create New Attack</Lang></h1>
         <TagsInput value={keywords} onChange={setKeywords} placeHolder="Search Keywords..." />
         <TagsInput value={domains} onChange={setDomains} placeHolder="Domains... (Optional)" />
         <Countries value={country} onChange={setCountry} />
+        {country && <Cities country={country} value={city} onChange={setCity} />}
     </Card>
 }
