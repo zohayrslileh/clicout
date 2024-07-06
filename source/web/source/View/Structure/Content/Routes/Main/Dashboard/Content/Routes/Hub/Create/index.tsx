89da0c7f805f2275d863e917e8899d16d272499a
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-06 16:08:19 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create/index.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create/index.tsx
@@ -30,7 +30,7 @@ export default function () {
      */
     const [country, setCountry] = useState<Country | undefined>(undefined)
 
-    return <Card className="grid gap-5 p-5 max-w-[700px] w-full h-fit mx-auto text-sm bg-background">
+    return <Card className="grid smooth gap-5 p-5 max-w-[700px] w-full h-fit mx-auto text-sm bg-content1">
         <h1 className="text-xl font-medium text-foreground-500 uppercase"><Lang>Create New Attack</Lang></h1>
         <TagsInput value={keywords} onChange={setKeywords} placeHolder="Search Keywords..." />
         <TagsInput value={domains} onChange={setDomains} placeHolder="Domains... (Optional)" />
