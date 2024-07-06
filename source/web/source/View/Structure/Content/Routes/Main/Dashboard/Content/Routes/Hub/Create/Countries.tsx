9939b549e2ead15c9f443eaac9d0463cc3086e77
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-06 16:52:00 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create/Countries.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create/Countries.tsx
@@ -1,6 +1,6 @@
 import { Autocomplete, AutocompleteItem } from "@nextui-org/react"
 import ReactCountryFlag from "react-country-flag"
-import { useEffect, useState } from "react"
+import { useCallback, useState } from "react"
 import usePromise from "@/Tools/Promise"
 import Country from "@/Core/Country"
 
@@ -15,7 +15,7 @@ export default function ({ value, onChange }: Props) {
      * Country code
      * 
      */
-    const [countryCode, setCountryCode] = useState<string | undefined>(value ? value.code : undefined)
+    const countryCode = value ? value.code : undefined
 
     /**
      * Items
@@ -44,27 +44,24 @@ export default function ({ value, onChange }: Props) {
     }, [keyword])
 
     /**
-     * On country code change
+     * Handle change
      * 
      */
-    useEffect(function () {
+    const handleChange = useCallback(function (key: string) {
 
         // Country
-        const country = items.find(country => country.code === countryCode)
-
-        // Set keyword
-        if (country) setKeyword(country.name)
+        const country = items.find(country => country.code === key)
 
         // On change callback
         onChange(country)
 
-    }, [countryCode])
+    }, [items])
 
     return <Autocomplete
         variant="bordered"
         startContent={countryCode ? <ReactCountryFlag countryCode={countryCode} svg /> : undefined}
         label="Select country" selectedKey={countryCode || ""}
-        onSelectionChange={key => setCountryCode(key ? key.toString() : "")}
+        onSelectionChange={key => handleChange(key ? key.toString() : "")}
         items={items}
         isLoading={countries.pending}
         onInputChange={setKeyword}
