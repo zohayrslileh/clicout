d30a551d34526ab89dfb2b67bd74ab62acdb24b1
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-06 12:00:33 +0100

    Global styles

--- source/web/source/View/Appearance/global.ts
+++ source/web/source/View/Appearance/global.ts
@@ -55,4 +55,24 @@ export default css`
 
         `} ease-out 500ms;
     }
+
+    /* width */
+    ::-webkit-scrollbar {
+      width: 10px;
+    }
+    
+    /* Track */
+    ::-webkit-scrollbar-track {
+      background: #f1f1f1; 
+    }
+    
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+      background: #888; 
+    }
+    
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+      background: #555; 
+    }
 `
