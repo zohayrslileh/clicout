1bd8ba8cf9418f73a10b91ebbb561daa3a8bccb9
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-06 13:39:27 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+import { TagsInput } from "react-tag-input-component";
 
-/**
- * Create
- * 
- * @returns
- */
-export default function () {
+const Example = () => {
+  const [selected, setSelected] = useState(["papaya"]);
 
-    return <div className="">
-        Create
+  return (
+    <div>
+      <h1>Add Fruits</h1>
+      <pre>{JSON.stringify(selected)}</pre>
+      <TagsInput
+        value={selected}
+        onChange={setSelected}
+        name="fruits"
+        placeHolder="enter fruits"
+      />
+      <em>press enter or comma to add new tag</em>
     </div>
-}
+  );
+};
+
+export default Example;
