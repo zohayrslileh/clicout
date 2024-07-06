fbf6ab606b956c225aacd54f7ef54937f7488ae5
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-06 17:34:49 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create/Countries.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Create/Countries.tsx
@@ -1,8 +1,8 @@
 import { Autocomplete, AutocompleteItem } from "@nextui-org/react"
 import ReactCountryFlag from "react-country-flag"
+import { useCallback, useState } from "react"
 import usePromise from "@/Tools/Promise"
 import Country from "@/Core/Country"
-import { useCallback } from "react"
 
 /**
  * Countries
@@ -18,28 +18,47 @@ export default function ({ value, onChange }: Props) {
     const countryId = value ? value.id : 0
 
     /**
+     * Items
+     * 
+     */
+    const [items, setItems] = useState<Country[]>([])
+
+    /**
+     * Keyword
+     * 
+     */
+    const [keyword, setKeyword] = useState("")
+
+    /**
      * Countries
      * 
      */
-    const countries = usePromise(async () => await Country.find(""), [])
+    const countries = usePromise(async function () {
+
+        // Countries
+        const countries = await Country.find(keyword)
+
+        // Set items
+        setItems(countries)
+
+    }, [keyword])
 
     /**
-     * Handle change method
+     * Handle change
      * 
-     * @returns
      */
     const handleChange = useCallback(function (id: number) {
 
-        // Check countries
-        if (!countries.solve) return
-
         // Country
-        const country = countries.solve.find(country => country.id === id)
+        const country = items.find(country => country.id === id)
+
+        // Set keyword
+        if (country) setKeyword(country.name)
 
         // On change callback
         onChange(country)
 
-    }, [countries.solve])
+    }, [items])
 
     return <Autocomplete
         variant="bordered"
@@ -47,11 +66,13 @@ export default function ({ value, onChange }: Props) {
         label="Select country"
         selectedKey={countryId.toString()}
         onSelectionChange={key => handleChange(key ? +key.toString() : 0)}
+        items={items}
         isLoading={countries.pending}
+        onInputChange={setKeyword}
     >
-        {(countries.solve || []).map(country => <AutocompleteItem key={country.id} value={country.id} startContent={<ReactCountryFlag countryCode={country.code} svg />}>
+        {country => <AutocompleteItem key={country.id} value={country.id} startContent={<ReactCountryFlag countryCode={country.code} svg />}>
             {country.name}
-        </AutocompleteItem>)}
+        </AutocompleteItem>}
     </Autocomplete>
 }
 
