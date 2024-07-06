6ba66e3e36a188c3df61d27b4208c13394173125
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-06 13:30:53 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record.tsx
@@ -13,7 +13,7 @@ export default function () {
     return <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
         <Card isPressable as={Link} to="create" className="relative h-[300px] smooth grid place-content-center place-items-center gap-5">
             <CiFlag1 className="text-3xl" />
-            <h1 className="uppercase"><Lang>Create New Attack</Lang></h1>
+            <p className="uppercase text-sm"><Lang>Create New Attack</Lang></p>
             <div className="absolute right-5 bottom-5 flex items-end gap-1 text-success-500">
                 <p className="text-3xl leading-none font-medium">0</p>
                 <p className="text-sm">/ 2 <Lang>Attack(s)</Lang></p>
