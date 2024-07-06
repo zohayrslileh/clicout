55c6548e3a010b21e6ac05205e2a534fd4a9c9c2
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-06 22:14:27 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Navbar/index.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Navbar/index.tsx
@@ -1,3 +1,4 @@
+import Card from "@/View/Components/Card"
 import Breadcrumbs from "./Breadcrumbs"
 import User from "./User"
 
@@ -8,11 +9,11 @@ import User from "./User"
  */
 export default function () {
 
-    return <div className="grid grid-cols-[1fr_auto] gap-3 px-5 py-3 items-center sticky z-50 top-0 smooth backdrop-blur-md p-3 border rounded-xl border-primary border-opacity-20">
+    return <Card circleStyle={false} curveStyle={false} className="grid grid-cols-[1fr_auto] gap-3 px-5 py-3 items-center sticky z-50 top-0 smooth backdrop-blur-md p-3">
 
         <Breadcrumbs />
 
         <User />
 
-    </div>
+    </Card>
 }
