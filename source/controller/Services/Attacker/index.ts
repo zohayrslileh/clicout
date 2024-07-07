31b1114f5ca8fa70571a33e21d7fc6e1409615e3
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-08 00:06:12 +0100

    Attacker Service

---
+++ source/controller/Services/Attacker/index.ts
@@ -0,0 +1,14 @@
+import Service from "@/Tools/Service"
+
+/*
+|-----------------------------
+|  Attacker Service
+|-----------------------------
+|
+|
+*/
+export default new Service("Server", async function () {
+
+
+    return "Attacker has started"
+})
