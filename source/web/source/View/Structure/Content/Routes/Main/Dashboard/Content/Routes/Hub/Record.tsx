a56278f7354fb9c448fdf89f92d2f398b5691c29
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-08 17:14:50 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record.tsx
@@ -22,9 +22,9 @@ export default function () {
             <Card circleStyle={false} className="relative active:scale-95 transition-all h-[300px] smooth grid place-content-center place-items-center gap-5">
                 <CiFlag1 className="text-3xl text-foreground-500" />
                 <p className="uppercase text-sm font-medium text-foreground-500"><Lang>Create New Attack</Lang></p>
-                <div className="absolute right-5 bottom-5 flex items-end gap-1 text-success-500">
-                    <p className="text-3xl leading-none font-medium">0</p>
-                    <p className="text-sm">/ {plan.threads} <Lang>Attack(s)</Lang></p>
+                <div className="absolute right-5 bottom-5 flex items-end gap-1 text-success-400">
+                    <p className="text-xl leading-none font-medium">0</p>
+                    <p className="text-[12px]">/ {plan.threads} <Lang>Attack(s)</Lang></p>
                 </div>
             </Card>
         </Link>
