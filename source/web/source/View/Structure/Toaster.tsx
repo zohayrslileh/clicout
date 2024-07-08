add0614dcd2ab044f1e25c67f5b1e848fb911996
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-08 22:56:43 +0100

    save

--- source/web/source/View/Structure/Toaster.tsx
+++ source/web/source/View/Structure/Toaster.tsx
@@ -1,4 +1,5 @@
-import { Toaster } from "react-hot-toast"
+import toast, { Toaster, useToasterStore } from "react-hot-toast"
+import { useEffect } from "react"
 
 /**
  * Toaster
@@ -6,6 +7,28 @@ import { Toaster } from "react-hot-toast"
  * @returns 
  */
 export default function () {
+
+    /**
+    * Toasts store
+    * 
+    */
+    const { toasts } = useToasterStore()
+
+    /**
+     * Limit toasts
+     * 
+     */
+    const TOAST_LIMIT = 3
+
+    /**
+     * On change toasts
+     * 
+     */
+    useEffect(() => {
+
+        toasts.filter((t, i) => t.visible && i >= TOAST_LIMIT && toast.dismiss(t.id))
+
+    }, [toasts])
 
     return <Toaster toastOptions={{
         style: {
