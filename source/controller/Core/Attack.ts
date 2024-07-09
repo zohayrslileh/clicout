6c96be051f54f726b18f20e24a3c7bd846ae35fd
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-09 18:48:53 +0100

    puppeteer

--- source/controller/Core/Attack.ts
+++ source/controller/Core/Attack.ts
@@ -109,7 +109,7 @@ export default class Attack extends EventEmitter {
 
         // Create browser
         const browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             args: ["--no-sandbox", "--disable-setuid-sandbox"]
         })
 
