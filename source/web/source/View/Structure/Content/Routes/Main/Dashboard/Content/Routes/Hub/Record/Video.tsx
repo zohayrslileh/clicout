3f38d8001d9101ef7e11525ba35fcdee90d8526c
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-11 16:10:40 +0100

    sass

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record/Video.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record/Video.tsx
@@ -9,7 +9,7 @@ import config from "@/config"
 export default function ({ attack }: Props) {
 
     return <video autoPlay>
-        <source src={`${config.DEV_SERVER}stream/${attack.id}`} />
+        <source src={`${import.meta.env.DEV ? config.DEV_SERVER : "/"}stream/${attack.id}`} />
         Your browser does not support HTML video.
     </video>
 }
