56ba3116bed3d5e4dbbbca9a063a12385ccf681c
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-11 22:35:00 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record/Video.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record/Video.tsx
@@ -1,7 +1,5 @@
-import webmHeader from "@/View/Media/webm-header.txt?raw"
-import { useCallback, useRef } from "react"
-import styled from "@emotion/styled"
 import Attack from "@/Core/Attack"
+import { useCallback, useRef } from "react"
 import User from "@/Core/User"
 
 /**
@@ -18,10 +16,22 @@ export default function ({ attack }: Props) {
     const namespace = User.useNamespace()
 
     /**
-     * Container
+     * Video 1
      * 
      */
-    const container = useRef<HTMLDivElement>(undefined!)
+    const video1 = useRef<HTMLVideoElement>(undefined!)
+
+    /**
+     * Video 2
+     * 
+     */
+    const video2 = useRef<HTMLVideoElement>(undefined!)
+
+    /**
+     * Video head
+     * 
+     */
+    const videoHead = useRef<ArrayBuffer | undefined>()
 
     /**
      * Chunks
@@ -35,58 +45,26 @@ export default function ({ attack }: Props) {
      */
     namespace.useOn(`${attack.id}:record-chunk`, async function (chunk: ArrayBuffer) {
 
-        // Clear
-        if (chunks.current.length > 10) chunks.current = []
+        // Set video head
+        if (!videoHead.current) return videoHead.current = chunk
 
-        // Push new chunk
-        chunks.current.push(chunk)
-
-        // Play
-        if (chunks.current.length >= 10) await play(chunks.current)
+        // Set chunks
+        chunks.current = [...chunks.current.slice(0, 20), chunk]
     })
 
     /**
-     * Play method
+     * Play
      * 
+     * @returns
      */
-    const play = useCallback(async function (items: ArrayBuffer[]) {
-
-        // Get videos
-        const videos = container.current.querySelectorAll("video")
-
-        // Last video
-        const lastVideo = videos[videos.length - 1]
-
-        // Check last video has end
-        if (lastVideo && !lastVideo.ended) return
-
-        // Revoke prev videos
-        for (const video of videos) URL.revokeObjectURL(video.src)
-
-        // Create video
-        const video = document.createElement("video")
-
-        // Create blob
-        const blob = new Blob([webmHeaderBuffer, ...items], { type: "video/webm" })
-
-        // Set video source
-        video.src = URL.createObjectURL(blob)
-
-        // Set current time
-        video.currentTime = 9999999
-
-        // Play
-        await video.play()
-
-        // Append to child
-        container.current.appendChild(video)
-
-        // Remove all and keep one
-        for (const video of [...videos].slice(0, -1)) video.remove()
+    const play = useCallback(function () {
 
     }, [])
 
-    return <Container ref={container} />
+    return <div>
+        <video ref={video1} controls />
+        <video ref={video2} controls />
+    </div>
 }
 
 /**
@@ -96,30 +74,3 @@ export default function ({ attack }: Props) {
 interface Props {
     attack: Attack
 }
-
-/**
- * Container
- * 
- */
-const Container = styled.div`
-    position: relative;
-
-    > video {
-        position: absolute;
-
-        &:last-child {
-            position: relative;
-        }
-    }
-`
-
-function base64ToArrayBuffer(base64: string) {
-    var binaryString = atob(base64);
-    var bytes = new Uint8Array(binaryString.length);
-    for (var i = 0; i < binaryString.length; i++) {
-        bytes[i] = binaryString.charCodeAt(i);
-    }
-    return bytes.buffer;
-}
-
-const webmHeaderBuffer = base64ToArrayBuffer(webmHeader)
