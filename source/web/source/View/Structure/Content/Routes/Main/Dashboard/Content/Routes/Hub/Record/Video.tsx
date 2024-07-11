340119a1bdc5f86eb1cfee2edb2d73d01a4df8a6
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-11 15:36:10 +0100

    save

--- source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record/Video.tsx
+++ source/web/source/View/Structure/Content/Routes/Main/Dashboard/Content/Routes/Hub/Record/Video.tsx
@@ -8,8 +8,8 @@ import config from "@/config"
  */
 export default function ({ attack }: Props) {
 
-    return <video autoPlay>
-        <source src={`${config.DEV_SERVER}stream/${attack.id}`} type="video/webm" />
+    return <video controls autoPlay>
+        <source src={`${config.DEV_SERVER}stream/${attack.id}`} />
         Your browser does not support HTML video.
     </video>
 }
