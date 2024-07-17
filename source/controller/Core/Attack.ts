1147b9f832e069fc217d8458171cd3265ee9aea2
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-17 15:58:32 +0100

    broadcast

--- source/controller/Core/Attack.ts
+++ source/controller/Core/Attack.ts
@@ -3,6 +3,7 @@ import SearchEntity from "@/Models/Database/Entities/Search"
 import Country, { PrimitiveCountry } from "./Country"
 import City, { PrimitiveCity } from "./City"
 import UserAgent from "./UserAgent"
+import EventEmitter from "events"
 import List from "@/Tools/List"
 import Search from "./Search"
 
@@ -14,6 +15,12 @@ import Search from "./Search"
 | 
 */
 export default class Attack {
+
+    /**
+     * Broadcast
+     * 
+     */
+    public static readonly broadcast = EventEmitter
 
     /**
      * Id
