23c6751504ff0735481c1daf6ad06d9bedf4337a
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-18 18:27:03 +0100

    save

--- source/controller/Console/Routes/tester.ts
+++ source/controller/Console/Routes/tester.ts
@@ -1,3 +1,5 @@
+import { writeFile } from "fs/promises"
+import puppeteer from "puppeteer"
 
 /*
 |-----------------------------
@@ -8,5 +10,34 @@
 */
 export default async function () {
 
-    console.log("The test completed successfully 🧪 ");
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox"
+        ]
+    })
+
+    const context = browser.defaultBrowserContext()
+
+    await context.overridePermissions("https://www.google.com", ["geolocation"])
+
+    const page = await browser.newPage()
+
+    await page.setViewport({ width: 1536, height: 730 })
+
+    const screencast = await page.screencast()
+
+    await page.goto("https://www.google.com/")
+
+    var i = 0
+
+    screencast.on("data", (chunk: Buffer) => {
+
+        i++
+
+        writeFile("storage/searches/" + i + ".txt", chunk.toString("hex").toUpperCase())
+    })
+
+    console.log("The test completed successfully 🧪 ")
 }
