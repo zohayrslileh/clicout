2ed67b682e055859174eaa309694ad202b4fc4ea
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-18 14:08:12 +0100

    save

--- source/controller/Services/Server/HTTP/Routes/stream.ts
+++ source/controller/Services/Server/HTTP/Routes/stream.ts
@@ -10,11 +10,14 @@ import { Context } from "hono"
 */
 export default async function (context: Context) {
 
-    // Set content type
-    context.header("Content-Type", "video/webm")
-
     // Record id
     const recordId = context.req.param("record_id")
+
+    // Set Content-Type
+    context.header("Content-Type", "video/webm")
+
+    // Set Transfer-Encoding
+    context.header("Transfer-Encoding", "chunked")
 
     // Create stream
     return stream(context, async function (stream) {
