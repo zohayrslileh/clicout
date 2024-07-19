9a83405120193ca25f76b5e0fa3c5337f7191ed6
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-19 15:31:56 +0100

    save

--- source/controller/Console/Routes/tester.ts
+++ source/controller/Console/Routes/tester.ts
@@ -1,6 +1,6 @@
+import { PassThrough } from "stream"
+import ffmpeg from "fluent-ffmpeg"
 import Json from "@/Tools/Json"
-import sleep from "@/Tools/Sleep"
-import puppeteer from "puppeteer"
 
 /*
 |-----------------------------
@@ -11,63 +11,17 @@ import puppeteer from "puppeteer"
 */
 export default async function () {
 
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox"
-        ]
-    })
+    const chunks = new Json<string[]>("storage/record.json").value
 
-    const context = browser.defaultBrowserContext()
+    const through = new PassThrough
 
-    await context.overridePermissions("https://www.google.com", ["geolocation"])
+    ffmpeg().input(through).output("storage/output.webm")
 
-    const page = await browser.newPage()
+    through.write(Buffer.from("1a45dfa3", "hex"))
 
-    page.setDefaultTimeout(0)
+    for (const chunk of chunks.slice(Math.round(chunks.length / 2) - 1)) through.write(Buffer.from(chunk, "hex"))
 
-    await page.setUserAgent("com.google.GoogleMobile/111.0 iPhone/13.5.1 hw/iPhone10_3")
-
-    await page.setViewport({ width: 375, height: 812 })
-
-    await page.setGeolocation({ latitude: -13.067464, longitude: -55.930092 })
-
-    const screencast = await page.screencast({ path: "storage/record.webm" })
-
-    const chunks: string[] = []
-
-    screencast.on("data", (chunk: Buffer) => chunks.push(chunk.toString("hex")))
-
-    await page.goto("https://www.google.com/search?q=apple")
-
-    await sleep(1000)
-
-    await page.goto("https://www.google.com/")
-
-    const textarea = await page.$("textarea")
-
-    if (!textarea) throw new Error
-
-    await sleep(2000)
-
-    await textarea.focus()
-
-    await textarea.type("Free vps")
-
-    await textarea.press("Enter")
-
-    await sleep(3000)
-
-    await page.goto("https://whatismyipaddress.com/ip-lookup")
-
-    await sleep(3000)
-
-    await screencast.stop()
-
-    await browser.close()
-
-    new Json<string[]>("storage/record.json").update(chunks)
+    through.end()
 
     console.log("The test completed successfully 🧪 ")
 }
