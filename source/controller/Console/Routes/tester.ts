ea5d896f560639d7f5f3a77339eaabeb3f001c5f
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-19 15:45:29 +0100

    save

--- source/controller/Console/Routes/tester.ts
+++ source/controller/Console/Routes/tester.ts
@@ -1,5 +1,3 @@
-import { PassThrough } from "stream"
-import ffmpeg from "fluent-ffmpeg"
 import Json from "@/Tools/Json"
 
 /*
@@ -11,17 +9,9 @@ import Json from "@/Tools/Json"
 */
 export default async function () {
 
-    const chunks = new Json<string[]>("storage/record.json").value
+    const [chunk] = new Json<string[]>("storage/record.json").value
 
-    const through = new PassThrough
-
-    ffmpeg().input(through).output("storage/output.webm")
-
-    through.write(Buffer.from("1a45dfa3", "hex"))
-
-    for (const chunk of chunks.slice(Math.round(chunks.length / 2) - 1)) through.write(Buffer.from(chunk, "hex"))
-
-    through.end()
+    console.log(chunk)
 
     console.log("The test completed successfully 🧪 ")
 }
