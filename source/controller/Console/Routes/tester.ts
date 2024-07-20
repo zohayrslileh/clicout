ffd3a4d9b2225ea9ec8eb777ea2ef34e5a9037af
Zohayr Slileh <zohayrslileh@gmail.com>
2024-07-20 12:41:17 +0100

    Tester 🧪

--- source/controller/Console/Routes/tester.ts
+++ source/controller/Console/Routes/tester.ts
@@ -1,7 +1,3 @@
-import { PuppeteerScreenRecorder } from "puppeteer-screen-recorder"
-import { PassThrough } from "stream"
-import puppeteer from "puppeteer"
-import sleep from "@/Tools/Sleep"
 
 /*
 |-----------------------------
@@ -11,72 +7,6 @@ import sleep from "@/Tools/Sleep"
 |
 */
 export default async function () {
-
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox"
-        ]
-    })
-
-    const context = browser.defaultBrowserContext()
-
-    await context.overridePermissions("https://www.google.com", ["geolocation"])
-
-    const page = await browser.newPage()
-
-    page.setDefaultTimeout(0)
-
-    await page.setUserAgent("com.google.GoogleMobile/111.0 iPhone/13.5.1 hw/iPhone10_3")
-
-    await page.setViewport({ width: 375, height: 812 })
-
-    await page.setGeolocation({ latitude: -13.067464, longitude: -55.930092 })
-
-    const through = new PassThrough()
-
-    through.on("data", data => console.log(data))
-
-    const recorder = new PuppeteerScreenRecorder(page)
-
-    recorder.startStream(through)
-
-    await page.goto("https://www.google.com/search?q=apple")
-
-    await sleep(1000)
-
-    await page.goto("https://www.google.com/")
-
-    const textarea = await page.$("textarea")
-
-    if (!textarea) throw new Error
-
-    await sleep(2000)
-
-    await textarea.focus()
-
-    await textarea.type("Free vps")
-
-    await textarea.press("Enter")
-
-    await sleep(3000)
-
-    await page.goto("https://whatismyipaddress.com/ip-lookup")
-
-    await sleep(3000)
-
-    await page.goto("https://whatismyipaddress.com/")
-
-    await sleep(3000)
-
-    await page.goto("https://whatismyipaddress.com/ip-lookup")
-
-    await sleep(3000)
-
-    await recorder.stop()
-
-    await browser.close()
 
     console.log("The test completed successfully 🧪 ")
 }
