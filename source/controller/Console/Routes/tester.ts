82c6f37329e7b3129f438f130e57789312b97e48
Zohayr Slileh <zohayrslileh@gmail.com>
2024-08-08 11:41:01 +0100

    Tester 🧪

--- source/controller/Console/Routes/tester.ts
+++ source/controller/Console/Routes/tester.ts
@@ -8,9 +8,5 @@
 */
 export default async function () {
 
-    const url = new URL("https://www.google.com/drive")
-
-    console.log(url.hostname)
-
     console.log("The test completed successfully 🧪 ")
 }
